5ca217549d9dd5c8817e840b617095f485d4f23a
akif_khan1 <akif_khan1@icloud.com>
2020-08-20 17:16:40 +0300

    udemy practice

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-## hackerrank 5th challenge ##
+                                   ## hackerrank 5th challenge ##
 
 # n = int(input(""))
 
@@ -8,23 +8,46 @@
 # for x in i:
 #     print (x*x)
 
-                                        ## 6th challenge ##
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0 and year % 400 == 0:
-            leap = True
-        elif year % 100 != 0 and year % 400 != 0:
-            leap = True
-        else:
-            leap = False
-    else:
-        leap = False
-    return leap
 
-year = int(input())
+#                                         # 6th challenge ##
 
-print (is_leap(year))
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0 and year % 400 == 0:
+#             leap = True
+#         elif year % 100 != 0 and year % 400 != 0:
+#             leap = True
+#         else:
+#             leap = False
+#     else:
+#         leap = False
+#     return leap
 
-                                          ## 7th challenge ##
+# year = int(input())
 
+# print (is_leap(year))
+
+
+ 
+#                                           # 7th challenge ##
+
+#         # solution from hackerrank ##
+# N = int(input())
+# print(*range(1,N+1), sep='')    # use of xrange function
+
+# print (*range(10,20+1), sep='')
+
+#                                           # 8th challenge ##
+
+# a = ("vadaptor server").rstrip('r')
+# # a.rstrip('n')
+# print (a)
+
+# a = [66,22,33,45,66,12,98,67,879,2333,66,12]
+# print (max(a))
+# print (a.count(66))
+
+# username = "Python3"
+# x = username.lower()
+# print (x)
