0e76ed858bd6fe3d0cfecad50e9f88f3fdf85127
akif_khan1 <akif_khan1@icloud.com>
2020-08-30 16:15:44 +0300

    list comprehension practice

--- test1.py
+++ test1.py
@@ -40,14 +40,107 @@
 
 #                                           # 8th challenge ##
 
-# a = ("vadaptor server").rstrip('r')
-# # a.rstrip('n')
-# print (a)
+       ## this solution is from hackerrank
+# import sys
+# import re
+# p = re.compile(r'(?<=\w)([\$\#\%\s]+)(?=\w)')
+# dem = sys.stdin.readline().split();
+# r = int(dem[0])
+# c = int(dem[1])
+# rows = [l for l in sys.stdin]
+# text = "";
+# for i in range(c):
+#     for j in range(r):
+#         text = text+rows[j][i]
+# print(p.sub(' ',text))
 
-# a = [66,22,33,45,66,12,98,67,879,2333,66,12]
-# print (max(a))
-# print (a.count(66))
 
-# username = "Python3"
-# x = username.lower()
-# print (x)
+                                        ## rstrip practice ##
+
+# a = ('my, name, is, atif, khan, i, m 35yrs old').rstrip(' imkhdyodrsl35,')
+# print (a)          # rstrip is a string method & start removing characters from the last until it found
+                     # a character which is not available in strip string.
+
+# n = ['a', 'b', 'c', 'd', 'e', 'f']
+# k = int(input(''))
+
+# print (n[0:999:5])
+
+# antiq = ('my name is khan')
+# print (antiq[0:999:2])
+
+                                   ## number game practice ##
+
+# n = 84
+
+# guesses = 1
+
+# print ('Number of guesses are limited to 9 times only!')
+
+# while guesses <= 9:
+#        n = int(input('enter a number! \n'))
+#        if n < 84:
+#               print ('your guess is less than winning number! \n')
+#        elif n > 84:
+#               print ('your guess is greater than winning number! \n')
+#        else:
+#               print ('$$$ you win $$$')
+#               print ('you took',guesses, 'guesses to win! ')
+#               break
+#        guesses = guesses + 1
+
+# if guesses > 9:
+#        print ('G@me OvEr!!!')
+       
+
+                                          ### hackerrank 9th challenge ###
+
+# n = []
+# n.append(1)
+# n.append(2)
+# n.append(3)
+# n.insert(2,9)
+# # n.index(1,0,999)
+
+# print (n.index(1,0,99))
+
+
+
+
+# namee = ['atif', 'ibrahim', 'afaf']
+# numee = [1,2,3,4]
+# tr = (namee.index('afaf',0,99))
+# namee.insert(tr,'kulsoom')
+# # namee.sort()
+# # namee.sort()
+# # namee.reverse()
+# print (namee)
+# namee.extend(numee)
+# # namee.pop(-2)
+# # namee.remove(1)
+# print (namee.index('atif'))
+# print (namee.count('atif'))
+
+
+
+# my_list = []
+
+# x = 1
+
+# while (x<11):
+#        y = my_list.append(int(input()))
+#        if y == 99:
+#               break
+#        x = x+1
+
+# print (my_list)
+
+                                                 #### list comprehension ###
+
+# print ([i for i in range (100) if i % 3 == 0])
+
+a = [1,2,3,4,5]
+b = ['a']
+
+a.extend(b)
+print (a)
